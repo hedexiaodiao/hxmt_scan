d88f734bc76930ec8c903de5abe8ccd8684552de
hedexiaodiao <woshiluoqi123@outlook.ocm>
2021-10-09 17:39:45 +0800

    add psf_math

---
+++ psf_HE/psf_math/psf_math.py
@@ -0,0 +1,12 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: psf_math.py
+# @Author: luoqi
+# @Institution: Institute of High Energy Physics, CAS
+# @E-mail: woshiluoqi123@outlook.com, luoqi@ihep.ac.cn
+# @Site: 
+# @Time: 10月 09, 2021
+# ---
+import numpy as np
